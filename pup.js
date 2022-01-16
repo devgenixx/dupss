4c7fd528f7cf46b0a1aede1d85fff312b8723ece
Anurag Yadav <75526793+devgenixx@users.noreply.github.com>
2022-01-16 11:04:56 +0530

    Add files via upload

---
+++ pup.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+{
+ $('.dog-slider').slick
+ ({
+    autoplay:true,
+    slidesToShow:3,
+    slidesToScroll:1,
+    prevArrow:".prev-btn",
+    nextArrow:".next-btn",
+ })
+});
